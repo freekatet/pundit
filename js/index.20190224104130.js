474bf228f7ed01e2b1b9839f74cc857764b710da
Freeka Tet <freeka.freeka@gmail.com>
2019-02-24 11:23:49 -0500

    Update index.20190224104130.js

--- js/index.20190224104130.js
+++ js/index.20190224104130.js
@@ -2,8 +2,6 @@
 $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
 
 function em1(){var c="nbtufsAqvoeju/gzj";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
-function em2(){var c="bmfyAqvoeju/gzj";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
-function em3(){var c="gsfflbAqvoeju/gzj";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
 
 $(document).ready(function() {
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
